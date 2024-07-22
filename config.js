f9c1fbfb5345b81596bdbca29a0575bc5518cdd1
Opaksfire <opakstg@gmail.com>
2024-07-22 01:25:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
+global.Owner = ["+2349013432338", "+2349013432338"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+2349013432338", "+2349013432338"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+2349013432338"];
 global.OwnerName = "SACHITH";
 global.BotName = "SACHITH";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
